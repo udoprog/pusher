efac7365f09fc1cae93f8b7bd5b80195356e7a0f
John-John Tedro <johnjohn.tedro@gmail.com>
2011-06-28 00:47:42 +0200

    Added the inspect command

---
+++ pusher/commands/inspect.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .base import ICommand
+from ..utils import TarFile
+
+from zope.interface import implements
+
+class InspectCommand:
+  implements(ICommand)
+
+  command = "inspect"
+
+  usage = "inspect [command]"
+  short = "Inspect information about the running environment"
+  docs  = """"""
+
+  def setenv(self, env):
+    self.env = env
+
+  def validate(self, args):
+    return args
+
+  def execute(self, *args):
+    for c in self.env.config:
+      print "{:20} = {}".format(c, self.env.config.get(c))
